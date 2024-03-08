23f6d7c66dc226738c958a788953a9422e32098d
Sameer Bhende <sameerbhende01@gmail.com>
2024-03-08 10:18:21 +0530

    Variables initialization
    Variables initialization

---
+++ 01_Variable.js
@@ -0,0 +1,13 @@
+var City;
+City="Pune";
+console.log(City);
+console.log("My city name is",City);
+
+console.log("What is undefined ?");
+var firstName; 
+console.log("First Name variable value before Initialization:", firstName);
+firstName="Gajanan";
+console.log("First name:", firstName);
+
+firstName = "Ratan";
+console.log("First name:", firstName);
---
+++ 01_variableAssign.js
@@ -0,0 +1,36 @@
+
+console.log("=====Assignment 1=====");
+
+var myFullName;
+myFullName = "Sameer Bhende";
+console.log(myFullName);
+myFullName = "Sameer Vidyadharrao Bhende ";
+console.log(myFullName);
+
+console.log("=====Assignment 2=====");
+
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("=====Assignment 3=====");
+
+var firstName="Sameer";
+console.log(firstName);
+
+var lastName="Bhende";
+console.log(lastName);
+
+var age=20;
+console.log(myLove);
+
+var address="Chincholi Dange";
+console.log(address);
+
+var pinCode=442201;
+console.log(pinCode);
+
+var gender="Male";
+console.log(gender);
+
+var IsMarried="Unmarried";
+console.log(IsMarried);
