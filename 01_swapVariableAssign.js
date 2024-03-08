fe97ba40111a75ab36e987d81e80305953c0db58
Sameer Bhende <sameerbhende01@gmail.com>
2024-03-08 11:54:52 +0530

    Swapping
    Swapping

---
+++ 01_swapVariableAssign.js
@@ -0,0 +1,29 @@
+console.log("ASSIGNMENT 1.1");
+console.log("WAP to swap variable values using third variable");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("======== Before Swap ==========");
+console.log("sweety:", sweety, " cutie:", cutie);
+
+console.log("======== After Swap ==========");
+var temp = sweety;
+sweety=cutie;
+cutie=temp;
+console.log("sweety:", sweety, " cutie:", cutie);
+console.log("==========");
+
+console.log("ASSIGNMENT 1.2");
+var n1=10;
+var n2=20;
+var n3=30;
+console.log("======== Before Swap ==========");
+console.log("n1:", n1, " n2:", n2,"n3:", n3);
+
+console.log("======== After Swap ==========");
+var temp=n1;
+n1=n3;
+n3=temp;
+console.log("n1:", n1, " n2:", n2,"n3:", n3);
+
+
+
