27b89cece23bf83fd1ae2a60d638c853befea778
Sameer Bhende <sameerbhende01@gmail.com>
2024-03-09 22:13:50 +0530

    02_typeOf.js
    02_typeOf.js

--- 02_typeOf.js
+++ 02_typeOf.js
@@ -6,53 +6,38 @@ console.log("Value of bankNAme is:", typeofAge);
 console.log("Type  of bankNAme is:", typeofAge);
 
 console.log("=======After Initialization======");
-var bankName="SBI_Bank";
-var typeofAge=typeof bankName;
+var bankName = "SBI_Bank";
+var typeofAge = typeof bankName;
 console.log("Value of bankNAme is:", bankName);
 console.log("Type  of bankNAme is:", typeofAge);
 
 console.log("*********Step 2********");
-var marks= "90%";
-var typeofMarks=typeof marks;
-console.log("Value of marks:",marks);
-console.log("type of marks:",typeofMarks);
-
+var marks = "90%";
+var typeofMarks = typeof marks;
+console.log("Value of marks:", marks);
+console.log("type of marks:", typeofMarks);
 
 console.log("*********Step 3********");
 console.log("=======Before Initialization======");
 var isWorking;
 var typeofisMarried = typeof isMarried;
-console.log("Value of isMarried:",typeofisMarried );
-console.log("Type of isMarried:",typeofisMarried );
+console.log("Value of isMarried:", typeofisMarried);
+console.log("Type of isMarried:", typeofisMarried);
 console.log("=======After Initialization======");
 
-var isMarried=true
-var typeofisMarried= typeof isMarried;
-console.log("Value of isMarried:",isMarried);
-console.log("type of isMarried",typeofisMarried);
+var isMarried = true;
+var typeofisMarried = typeof isMarried;
+console.log("Value of isMarried:", isMarried);
+console.log("type of isMarried", typeofisMarried);
 
 console.log("*********Step 4********");
-var totalCount= "One hundred seven";
-var typeoftotalCount=typeof totalCount;
+var totalCount = "One hundred seven";
+var typeoftotalCount = typeof totalCount;
 console.log("Value of totalCount:", totalCount);
 console.log("Type of totalCount:", typeoftotalCount);
 console.log("=======After update variable======");
 
-var totalCount=107;
-var typeoftotalCount= typeof totalCount;
+var totalCount = 107;
+var typeoftotalCount = typeof totalCount;
 console.log("Value of totalCount:", totalCount);
 console.log("Type of totalCount:", typeoftotalCount);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
