f1d85f8dcc3817456eb8b3ffdb7a86e7162ef857
Sameer Bhende <sameerbhende01@gmail.com>
2024-03-12 22:42:34 +0530

    03_string BasicAssign.js
    03_string BasicAssign.js

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,20 @@
+
+console.log("====Step 1====");
+function stringBasic(Company)
+{
+     var company=Company;
+     console.log(`My favourite Company: ${company}`);
+}
+  stringBasic("Codemind");
+
+  console.log("====Step 2====");
+
+  var h1="Learning";
+  var h2= "playing";
+  var h3= "swimming";
+  console.log(`My favourite hobbies are:${h1},${h2},and ${h3}`);
+
+  console.log("====Step 3====");
+  var hobby= "Swimming, learning, playing";
+  console.log(`The length of ${hobby} is :${hobby.length}`);
+
---
+++ 04_stringTemplate.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+console.log(`I am "Software" Developer`);
+
+var city = "Pune";
+var firstName = "Gajanan";
+
+//console.log("My name is: ", firstName, " and city is: ", city);
+
+console.log(` My name is: ${firstName} and city is: ${city} `);//String Template
+
+
+
+console.log("========== replace() ==================");
+var greet = "Good Morning";
+const afterReplace = greet.replace("Morning", "Afternoon");
+console.log(`After replace string is: ${afterReplace}`);
+
+
+console.log(`========== toUpperCase() ==================`);
+var greet = "Good Morning";
+var greetInUpperCase = greet.toUpperCase();
+console.log(`${greet} in upper case is : ${greetInUpperCase}`);
